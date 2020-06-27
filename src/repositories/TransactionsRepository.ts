f9180426f1ed6f93a8bb475834f12193134a9c8a
Maikon Germano <mk_germano@hotmail.com>
2020-06-27 14:11:19 -0300

    'ok'

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,28 +14,31 @@ class TransactionsRepository extends Repository<Transaction> {
     // TODO
     const transaction = await this.find();
 
-    const {income, outcome} = transaction.reduce((accumulador, transaction) => {
-      switch(transaction.type){
-        case 'income':
-          accumulador.income += Number(transaction.value);
-          break;
-        case 'outcome':
-          accumulador.outcome += Number(transaction.value);
-          break;
-        default:
-          break;
-      }
-      
-      return accumulador;
-    },{
-      income: 0,
-      outcome: 0,
-      total: 0,
-    });
+    const { income, outcome } = transaction.reduce(
+      (accumulador, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulador.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            accumulador.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+
+        return accumulador;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
 
     const total = income - outcome;
 
-    return {income, outcome, total};
+    return { income, outcome, total };
   }
 }
 
